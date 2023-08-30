b2b3f7515da24b0e5fd9cdc7a6a7fc571834c9ea
David Sanchez <davidsanrubio@gmail.com>
2023-08-30 21:54:56 +0200

    chore: Add vecel static build to website

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -3,15 +3,14 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { fileURLToPath } from 'node:url';
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/packages/website',
-  integrations: [
-    react(),
-    tailwind({
-      configFile: fileURLToPath(
-        new URL('./tailwind.config.cjs', import.meta.url)
-      ),
-    }),
-  ],
+  integrations: [react(), tailwind({
+    configFile: fileURLToPath(new URL('./tailwind.config.cjs', import.meta.url))
+  })],
+  output: 'static',
+  adapter: vercel()
 });
